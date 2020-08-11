0911b66e073acbbe121e3c44c31bc260b40ff8c1
Yuri Eastwood <yuri.eastwood@live.com>
2020-08-10 21:29:10 -0300

    First version of Nubank extractor regex (old statement model, early 2020)

--- CreditCardPdfStatementExtractor/Program.cs
+++ CreditCardPdfStatementExtractor/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UglyToad.PdfPig;
 
 namespace CreditCardPdfStatementExtractor
 {
@@ -6,7 +11,37 @@ namespace CreditCardPdfStatementExtractor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var root = @"/Users/yurieastwood/Downloads";
+            var folders = new List<string>{ "cartao-nubank", "cartao-itau-5827", "cartao-itau-6788" };
+
+            foreach (var folder in folders)
+            {
+                var files = Directory.GetFileSystemEntries(Path.Combine(root, folder));
+
+                foreach (var file in files)
+                {
+                    using var document = PdfDocument.Open(file);
+                    foreach (var page in document.GetPages())
+                    {
+                        var pageText = page.Text;
+                        //Console.WriteLine($"Page {page.Number}: {pageText}");
+
+                        if (pageText.Contains("TRANSAÇÕES"))
+                        {
+                            Console.WriteLine($"{file.Split('/').Last()}");
+
+                            var text = pageText.Split("VALORES EM R$");
+                            Console.WriteLine($"{text[1]}");
+                            var regex = new Regex(@"(\d{2}\s{1}(?:JAN|FEV|MAR|ABR|MAI|JUN|JUL|AGO|SET|OUT|NOV|DEZ))(.*?)(?<!(?:USD|BRL|R\$)\s.?)((?:\d+\.)*?\d+,\d{2})");
+                            foreach (Match match in regex.Matches(text[1]))
+                                Console.WriteLine($"{match.Groups[1].Value};{match.Groups[2].Value.Trim()};{match.Groups[3].Value}");
+                        }
+
+                    }
+                }
+
+                return;
+            }
         }
     }
 }
